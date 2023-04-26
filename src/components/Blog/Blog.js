dd032bffca0d7fc69c42b2db066e0b3c8cc3fde3
StaniWills <deberkader@mail.ru>
2023-04-26 15:08:04 +0200

    neue project

---
+++ src/components/Blog/Blog.js
@@ -0,0 +1,8 @@
+import React from 'react'
+import s from './Blog.module.css'
+
+export default function Blog() {
+  return (
+    <div>Blog</div>
+  )
+}
---
+++ src/components/Work/Work.js
@@ -0,0 +1,8 @@
+import React from 'react'
+import s from './Work.module.css' 
+
+export default function Work() {
+  return (
+    <div>Work</div>
+  )
+}
